04ae9efa72e5083b02c0fb5725c8ad81ec5d2f56
saurvkmr <contact.xenon@gmail.com>
2020-12-28 22:26:35 +0530

    controller updated

--- src/main/java/learn/springboot/controller/RestApiDemoController.java
+++ src/main/java/learn/springboot/controller/RestApiDemoController.java
@@ -1,8 +1,8 @@
 package learn.springboot.controller;
 
 import learn.springboot.model.Coffee;
+import learn.springboot.repository.CoffeeRepository;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,17 +12,16 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 import java.util.Optional;
 
 @RestController("/coffee")
 public class RestApiDemoController {
-    private List<Coffee> coffees = new ArrayList<>();
+    CoffeeRepository coffeeRepository;
 
-    public RestApiDemoController() {
-        coffees.addAll(Arrays.asList(
+    public RestApiDemoController(CoffeeRepository coffeeRepository) {
+        this.coffeeRepository = coffeeRepository;
+        this.coffeeRepository.saveAll(Arrays.asList(
                 new Coffee("Café Cereza"),
                 new Coffee("Café Ganador"),
                 new Coffee("Café Lareño"),
@@ -32,33 +31,29 @@ public class RestApiDemoController {
 
     @GetMapping
     ResponseEntity<Iterable<Coffee>> getCoffee() {
-        return new ResponseEntity<>(coffees, HttpStatus.OK);
+        return new ResponseEntity<>(coffeeRepository.findAll(), HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
     ResponseEntity<Optional<Coffee>> getCoffeeById(@PathVariable String id) {
-        return new ResponseEntity<>
-                (coffees.stream().filter(coffee -> coffee.getId().equalsIgnoreCase(id)).findFirst(), HttpStatus.OK);
+        return new ResponseEntity<>(this.coffeeRepository.findById(id), HttpStatus.OK);
     }
 
     @PostMapping
     ResponseEntity<Coffee> postCoffee(@RequestBody Coffee coffee) {
-        coffees.add(coffee);
-        return new ResponseEntity<>(coffee, HttpStatus.CREATED);
+        return new ResponseEntity<>(this.coffeeRepository.save(coffee), HttpStatus.CREATED);
     }
 
     @PutMapping("/{id}")
     ResponseEntity<Coffee> putCoffee(@PathVariable String id, @RequestBody Coffee coffee) {
-        Optional<Coffee> opt = coffees.stream()
-                .filter(coffee1 -> coffee1.getId().equalsIgnoreCase(id))
-                .findFirst();
-        return opt.isPresent()
+        return coffeeRepository.existsById(id)
                 ? new ResponseEntity<>(coffee, HttpStatus.OK)
                 : postCoffee(coffee);
     }
 
     @DeleteMapping("/{id}")
-    ResponseEntity<Boolean> deleteCoffee(@PathVariable String id) {
-        return new ResponseEntity<>(coffees.removeIf(c -> c.getId().equalsIgnoreCase(id)), HttpStatus.OK);
+    ResponseEntity<Void> deleteCoffee(@PathVariable String id) {
+        coffeeRepository.deleteById(id);
+        return new ResponseEntity(HttpStatus.OK);
     }
 }
