57f43e1937a3c0964240636f96c2ec4f90ed312b
saurvkmr <contact.xenon@gmail.com>
2021-01-24 11:48:54 +0530

    update

---
+++ src/test/java/funWithFluxMono/VirtualTimeSchedulerTest.kt
@@ -0,0 +1,22 @@
+package funWithFluxMono
+
+import org.junit.jupiter.api.Test
+import reactor.core.publisher.Flux
+import reactor.test.StepVerifier
+import reactor.test.scheduler.VirtualTimeScheduler
+import java.time.Duration
+
+class VirtualTimeSchedulerTest {
+    @Test
+    fun virtualTimeSchedulerTest() {
+        VirtualTimeScheduler.getOrSet()
+        val fluxWithDelay = Flux.just(1, 2, 3, 4, 5, 6, 7, 8, 9)
+            .delayElements(Duration.ofSeconds(1))
+
+        StepVerifier.withVirtualTime { fluxWithDelay.log() }
+            .expectSubscription()
+            .thenAwait(Duration.ofSeconds(9))
+            .expectNext(1, 2, 3, 4, 5, 6, 7, 8, 9)
+            .verifyComplete()
+    }
+}
