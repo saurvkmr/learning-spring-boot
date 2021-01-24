65df146a7e329e60b7e81414994da90e818626a4
saurvkmr <contact.xenon@gmail.com>
2021-01-24 12:21:47 +0530

    update

---
+++ src/main/java/com/springReactive/controller/FluxController.kt
@@ -0,0 +1,32 @@
+package com.springReactive.controller
+
+import org.springframework.http.MediaType
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import reactor.core.publisher.Flux
+import java.time.Duration
+
+@RestController
+@RequestMapping("/flux")
+class FluxController {
+    @GetMapping
+    fun getFlux(): Flux<Int> {
+        return Flux.just(1,2,3,4).log()
+    }
+
+    @GetMapping("/range")
+    fun getFluxWithInterval(): Flux<Int> {
+        return Flux.range(0,10)
+            .delayElements(Duration.ofSeconds(1))
+            .log()
+    }
+
+    @GetMapping("/rangeStream", produces = [MediaType.APPLICATION_NDJSON_VALUE])
+    fun getFluxWithIntervalStream(): Flux<Int> {
+        return Flux.range(0,10)
+            .delayElements(Duration.ofSeconds(1))
+            .log()
+    }
+
+}
