e8c94d75056a91ad21d6e9170d651bdf2e3e8f3a
saurvkmr <contact.xenon@gmail.com>
2021-01-25 16:41:16 +0530

    update

--- src/main/java/com/springReactive/repo/ItemReactive.kt
+++ src/main/java/com/springReactive/repo/ItemReactive.kt
@@ -2,6 +2,9 @@ package com.springReactive.repo
 
 import com.springReactive.document.Item
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository
+import reactor.core.publisher.Flux
 import java.util.*
 
-interface ItemReactive : ReactiveMongoRepository<Item, UUID> {}
+interface ItemReactive : ReactiveMongoRepository<Item, UUID> {
+    fun findByDesc(desc: String): Flux<Item>
+}
--- src/test/java/com/springReactive/repo/ItemReactiveTest.kt
+++ src/test/java/com/springReactive/repo/ItemReactiveTest.kt
@@ -19,7 +19,8 @@ class ItemReactiveTest {
     private val items = arrayListOf(Item(UUID.randomUUID(), "Samsung TV", 4.5F),
         Item(UUID.randomUUID(), "Apple Watch", 14.5F),
         Item(UUID.randomUUID(), "OnePlus", 24.5F),
-        Item(UUID.randomUUID(), "Tata Car", 64.5F),
+        Item(UUID.randomUUID(), "Tata Car", 64.5F),//47faa51c-f270-4564-a989-4300d5205c8d
+        Item(UUID.fromString("47faa51c-f270-4564-a989-4300d5205c8d"), "Airpod", 98.9F)
     )
 
     /*private val items = arrayListOf(Item(null, "Samsung TV", 4.5F),
@@ -42,7 +43,49 @@ class ItemReactiveTest {
     fun findAllElements() {
         StepVerifier.create(itemReactive.findAll())
             .expectSubscription()
-            .expectNextCount(4)
+            .expectNextCount(5)
             .verifyComplete()
+    }
+
+    @Test
+    fun getItemById() {
+        StepVerifier.create(itemReactive.findById(UUID.fromString("47faa51c-f270-4564-a989-4300d5205c8d")))
+            .expectSubscription()
+            .expectNextMatches { it.price ==  98.9F}
+            .verifyComplete()
+    }
+
+    @Test
+    fun getItemByDesc() {
+        StepVerifier.create(itemReactive.findByDesc("Airpod").log())
+            .expectSubscription()
+            .expectNextMatches { it.price ==  98.9F}
+            .verifyComplete()
+    }
+
+    @Test
+    fun saveItem() {
+        val item = Item(UUID.randomUUID(), "BMW", 64.5F)
+        StepVerifier.create(itemReactive.save(item))
+            .expectSubscription()
+            .expectNextCount(1)
+            .expectNextMatches { it.desc == "BMW" }
+            .expectComplete()
+
+        itemReactive.delete(item)
+    }
+
+    @Test
+    fun updateItem() {
+        val flux = itemReactive.findByDesc("OnePlus")
+            .map { it.price = 99F; return@map it }
+            .flatMap { itemReactive.save(it); return@flatMap Flux.just(it) }
+
+        StepVerifier.create(flux.log())
+            .expectSubscription()
+            .expectNextCount(1)
+            //.expectNextMatches { it.price == 99.0F }
+            .verifyComplete()
+
     }
 }
